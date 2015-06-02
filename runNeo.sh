d21e60a36a89a2bab358c018f7b73b31bbe677f5
LucasFLima <lucas.f.lima@gmail.com>
2015-06-02 12:01:07 -0300

    Include path to utils scripts

--- runNeo.sh
+++ runNeo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-out=`cat parametros | grep "out_dir:" | cut -d":" -f2`
-neo_dir=`cat parametros | grep "neo_dir:" | cut -d":" -f2`
-module=`cat parametros | grep "module:" | cut -d":" -f2`
-target=`cat parametros | grep "target:" | cut -d":" -f2`
+utilPath="/home/lucas/workspace/utilLucasFLima"
+
+out=`cat $utilPath/parametros | grep "out_dir:" | cut -d":" -f2`
+neo_dir=`cat $utilPath/parametros | grep "neo_dir:" | cut -d":" -f2`
+module=`cat $utilPath/parametros | grep "module:" | cut -d":" -f2`
+target=`cat $utilPath/parametros | grep "target:" | cut -d":" -f2`
 
 echo "- - - - - - - - - - - - - - - - -"
 echo "Modulo: "$module
