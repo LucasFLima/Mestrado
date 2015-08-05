df49cace1413f9aa29d6b83cbea08c8505110f33
Lucas F. Lima <lucas.f.lima@gmail.com>
2015-08-05 10:57:25 -0300

    organizacao

--- commands/cleanOut.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-utilPath="/home/lucas/workspace/utilLucasFLima"
-out=`cat $utilPath/parametros | grep "out_dir:" | cut -d":" -f2`
-
-find $out/* -maxdepth 1 -exec echo "Removing " {} \; -exec rm {} \;
-echo $out" cleaned"
