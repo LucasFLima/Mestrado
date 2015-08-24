c2e1d49773f121d0f2e249e860757bf4147f15db
Lucas Lima <lucas.f.lima@gmail.com>
2015-08-24 01:47:44 -0300

    contratos convertidos para NeoIDL

---
+++ parserPerl/executaTodos.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+echo "executando contratosExercito/Agendador/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Agendador/synapse.json > parserPerl/out/Agendador
+echo "executando contratosExercito/Alerta/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Alerta/synapse.json > parserPerl/out/Alerta
+echo "executando contratosExercito/Anexo/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Anexo/synapse.json > parserPerl/out/Anexo
+echo "executando contratosExercito/Assinatura/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Assinatura/synapse.json > parserPerl/out/Assinatura
+echo "executando contratosExercito/Associacao/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Associacao/synapse.json > parserPerl/out/Associacao
+echo "executando contratosExercito/Backup/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Backup/synapse.json > parserPerl/out/Backup
+echo "executando contratosExercito/CacheAccess/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/CacheAccess/synapse.json > parserPerl/out/CacheAccess
+echo "executando contratosExercito/CalcoNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/CalcoNeuronSAD/synapse.json > parserPerl/out/CalcoNeuronSAD
+echo "executando contratosExercito/CalungaNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/CalungaNeuronSAD/synapse.json > parserPerl/out/CalungaNeuronSAD
+echo "executando contratosExercito/Certificado/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Certificado/synapse.json > parserPerl/out/Certificado
+echo "executando contratosExercito/ChatEnvio/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/ChatEnvio/synapse.json > parserPerl/out/ChatEnvio
+echo "executando contratosExercito/Chat/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Chat/synapse.json > parserPerl/out/Chat
+echo "executando contratosExercito/ComintNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/ComintNeuronSAD/synapse.json > parserPerl/out/ComintNeuronSAD
+echo "executando contratosExercito/ComunicacaoInterface/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/ComunicacaoInterface/synapse.json > parserPerl/out/ComunicacaoInterface
+echo "executando contratosExercito/Comunicacao/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Comunicacao/synapse.json > parserPerl/out/Comunicacao
+echo "executando contratosExercito/Configuracao/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Configuracao/synapse.json > parserPerl/out/Configuracao
+echo "executando contratosExercito/DispositivoRastreamento/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/DispositivoRastreamento/synapse.json > parserPerl/out/DispositivoRastreamento
+echo "executando contratosExercito/DominioNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/DominioNeuronSAD/synapse.json > parserPerl/out/DominioNeuronSAD
+echo "executando contratosExercito/EventoNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/EventoNeuronSAD/synapse.json > parserPerl/out/EventoNeuronSAD
+echo "executando contratosExercito/InmetNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/InmetNeuronSAD/synapse.json > parserPerl/out/InmetNeuronSAD
+echo "executando contratosExercito/Instancias/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Instancias/synapse.json > parserPerl/out/Instancias
+echo "executando contratosExercito/ItemNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/ItemNeuronSAD/synapse.json > parserPerl/out/ItemNeuronSAD
+echo "executando contratosExercito/LocalizadorFiltro/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/LocalizadorFiltro/synapse.json > parserPerl/out/LocalizadorFiltro
+echo "executando contratosExercito/LocalizadorNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/LocalizadorNeuronSAD/synapse.json > parserPerl/out/LocalizadorNeuronSAD
+echo "executando contratosExercito/MapaNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/MapaNeuronSAD/synapse.json > parserPerl/out/MapaNeuronSAD
+echo "executando contratosExercito/MapeamentoRadar/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/MapeamentoRadar/synapse.json > parserPerl/out/MapeamentoRadar
+echo "executando contratosExercito/Mensagem/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Mensagem/synapse.json > parserPerl/out/Mensagem
+echo "executando contratosExercito/Movimentacao/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Movimentacao/synapse.json > parserPerl/out/Movimentacao
+echo "executando contratosExercito/OrdemBatalha/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/OrdemBatalha/synapse.json > parserPerl/out/OrdemBatalha
+echo "executando contratosExercito/PlanoOrdem/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/PlanoOrdem/synapse.json > parserPerl/out/PlanoOrdem
+echo "executando contratosExercito/ProvedorSeguranca/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/ProvedorSeguranca/synapse.json > parserPerl/out/ProvedorSeguranca
+echo "executando contratosExercito/RadarBuffer/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/RadarBuffer/synapse.json > parserPerl/out/RadarBuffer
+echo "executando contratosExercito/RadarNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/RadarNeuronSAD/synapse.json > parserPerl/out/RadarNeuronSAD
+echo "executando contratosExercito/Radio/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Radio/synapse.json > parserPerl/out/Radio
+echo "executando contratosExercito/Receptor/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Receptor/synapse.json > parserPerl/out/Receptor
+echo "executando contratosExercito/RedeRelacionamento/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/RedeRelacionamento/synapse.json > parserPerl/out/RedeRelacionamento
+echo "executando contratosExercito/Revisualizacao/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Revisualizacao/synapse.json > parserPerl/out/Revisualizacao
+echo "executando contratosExercito/SGL/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/SGL/synapse.json > parserPerl/out/SGL
+echo "executando contratosExercito/Simbolo/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Simbolo/synapse.json > parserPerl/out/Simbolo
+echo "executando contratosExercito/Tile/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Tile/synapse.json > parserPerl/out/Tile
+echo "executando contratosExercito/TipoNeuronSAD/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/TipoNeuronSAD/synapse.json > parserPerl/out/TipoNeuronSAD
+echo "executando contratosExercito/Trajetoria/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/Trajetoria/synapse.json > parserPerl/out/Trajetoria
+echo "executando contratosExercito/UsuariosChat/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/UsuariosChat/synapse.json > parserPerl/out/UsuariosChat
+echo "executando contratosExercito/XMLHighlighter/synapse.json"
+parserPerl/swagger2NeoIDL.pl contratosExercito/XMLHighlighter/synapse.json > parserPerl/out/XMLHighlighter
---
+++ parserPerl/montaExecuta.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo '#!/bin/bash'; find contratosExercito/*/synapse.json -name synapse.json -exec sh -c "echo 'echo \"executando {}\"'; echo -n 'parserPerl/swagger2NeoIDL.pl '; echo -n {}' > parserPerl/out/'; echo -n {} | cut -f2 -d'/';" \;
