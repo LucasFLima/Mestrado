4843be0a4e14e78e7848d6b2496ae2d91e970f4c
Lucas Lima <lucas.f.lima@gmail.com>
2015-10-28 10:53:48 -0200

    include metafiles in ingore list

--- eclipse/workspace_python/.metadata/.plugins/org.python.pydev.shared_interactive_console/history.py
+++
@@ -1,6 +0,0 @@
-import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
-request.pattenr
-request.pattern
-import sys; print('%s %s' % (sys.executable or sys.platform, sys.version))
-pattern
-request.path
