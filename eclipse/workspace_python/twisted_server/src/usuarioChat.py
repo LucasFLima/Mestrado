f2a1573d2ca47ca9d0781ec2bbc226768fa12d23
Lucas Lima <lucas.f.lima@gmail.com>
2015-11-03 09:33:50 -0200

    Versão alvo para primeira etapa sem suporte a dbc

--- eclipse/workspace_python/twisted_server/src/usuarioChat.py
+++ eclipse/workspace_python/twisted_server/src/usuarioChat.py
@@ -9,65 +9,37 @@ class Recibo (object):
         self.destinatario = destinatario
         self.dataHora = dataHora
 
-class GetRequestParameters:
+class GetRequestParameters(object):
     def __init__ (self, token, nomeInstancia, idUsuarioDestino):
         self.token = token
         self.nomeInstancia = nomeInstancia
         self.idUsuarioDestino = idUsuarioDestino
 
-class Chamada (object):
-    retorno = ''
-    
-    def __init__ (self, url = None):
-        self.url = url
-        
-    def call(self):
-        r=HTTPConnection("127.0.0.1", 8080)
-        r.request('GET', '/hello')
-        res = r.getresponse()
-        self.retorno = res.status
-        
-
 class Resource(object):
-    @classmethod
-    def processaChamada (rCode):
-        
-        rCode = rCode
     
     @classmethod
     def get(cls, request):
         
-        #set variables used in capability
-        responseCode = ResponseCode.Ok
-        getRequestParameters = GetRequestParameters (request.args['t'], request.args['nomeInstancia'], request.args['idUsuarioDestino'])
+#         #set variables used in capability
+        responseCode = ResponseCode.Ok 
+#         getRequestParameters = GetRequestParameters (request.args['t'], request.args['nomeInstancia'], request.args['idUsuarioDestino'])
+#         
+#         if getRequestParameters.token == 'TOKEN':
+#             responseCode = ResponseCode.InvalidPrecondition
+#             return responseCode, 'Invalid Precondition'
+#         
+#         if getRequestParameters.idUsuarioDestino != "usuarioY":
+#             responseCode = ResponseCode.Unauthorized
+#             return responseCode, 'Unauthorized User'
         
-        if getRequestParameters.token == 'TOKEN':
-            responseCode = ResponseCode.InvalidPrecondition
-            return responseCode, 'Invalid Precondition'
-        
-        if getRequestParameters.idUsuarioDestino != "usuarioY":
-            responseCode = ResponseCode.Unauthorized
-            return responseCode, 'Unauthorized User'
-        
-        #url1 = 'http://localhost:8080/usuariosChat/instancia/instanciaX/mensagem/usuarioX/usuarios?t=TOKENOK'
-        #callService(url1, Resource.processaChamada)
-#         try:
-#             r=HTTPConnection("127.0.0.1", 8080, timeout=2)
-#             r.request('GET', '/hello')
-#             res = r.getresponse()
-#             statusCode = res.status
-#         except Exception as e:
-#             print e
-#             statusCode = 0
-        x = Chamada()
-        x.call()
+
       
         #######    Replace this section by your logic   #######
         result = {}
         result ['method'] = 'GET'
         result ['path'] = request.path
         result ['arguments'] = request.args
-        result ['statusCode'] = x.retorno
+        
         
         return responseCode, json.dumps(result, sort_keys=False, indent=4, separators=(',', ': '))
         #######    Replace this section by your logic   #######
@@ -89,6 +61,7 @@ class Resource(object):
              
         
         #######    Replace this section by your logic   #######
+        responseCode = ResponseCode.Ok
         result = {}
         result ['method'] = 'POST'
         result ['path'] = request.path
@@ -100,6 +73,7 @@ class Resource(object):
     @classmethod
     def put(cls, request):
         
+        responseCode = ResponseCode.Ok
         result = {}
         result ['method'] = 'PUT'
         result ['path'] = request.path
@@ -110,6 +84,7 @@ class Resource(object):
     @classmethod
     def delete(cls, request):
         
+        responseCode = ResponseCode.Ok
         result = {}
         result ['method'] = 'DELETE'
         result ['path'] = request.path
