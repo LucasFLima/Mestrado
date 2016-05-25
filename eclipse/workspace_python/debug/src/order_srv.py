c03276e50e726eea71e2e90d0575b5d8cb9c396f
Lucas Lima <lucas.f.lima@gmail.com>
2016-05-25 14:35:11 -0300

    Callback chain now works

--- eclipse/workspace_python/debug/src/order_srv.py
+++ eclipse/workspace_python/debug/src/order_srv.py
@@ -1,10 +1,11 @@
 import json
-import twisted_server
 from twisted_server import ResponseCode
 from httplib import HTTPConnection
 import pre_order_srv
 from pre_order_srv import PreConditionOrder
 from twisted.web.server import NOT_DONE_YET
+from twisted.internet import reactor, defer
+import teste3
  
 class GetRequestParameters(object):
     def __init__ (self, xxx, yyy, zzz):
@@ -19,70 +20,85 @@ class PostRequestParameters(object):
         self.zzz = zzz
  
 class Resource(object): 
- 
-    def getProcessing (self, message, request):
+     
+    @classmethod
+    def preGetProcessing (self, result, request):
         #######    Replace this section by your logic   #######
         responseCode = ResponseCode.Ok
         
-        result = {}
-        result ['method'] = 'post'
-        result ['path'] = request.path
-        result ['arguments'] = request.args
-        responseBody = json.dumps(result, sort_keys=False, indent=4, separators=(',', ': '))
+        print ">> preGetProcessing"
+        resultData = {}
+        resultData ['method'] = 'get'
+        resultData ['path'] = request.path
+        resultData ['arguments'] = request.args
+        responseBody = json.dumps(resultData, sort_keys=False, indent=4, separators=(',', ': '))
         #######    Replace this section by your logic   #######
+       
+        #raise RuntimeError, "whoops! we encountered an error"
+
+        return responseCode, responseBody
+
+    @classmethod
+    def preGetOtherwise (self, f, dbc1):
+        #######    Replace this section by your logic   #######
+        responseCode = ResponseCode.InternalServerError
+        
+        f.trap(RuntimeError)
+        
+        print ">> preGetOthewise"
+        responseBody = "Falha"
+        #######    Replace this section by your logic   #######
+        dbc1.callbacks = []
+        return responseCode, responseBody
+
+
+    
+    @classmethod
+    def getProcessing (self, result, request):
+        #######    Replace this section by your logic   #######
+        responseCode = ResponseCode.Ok
+
+        print ">> getProcessing"
+        
+        resultData = {}
+        resultData ['method'] = 'get'
+        resultData ['path'] = request.path
+        resultData ['arguments'] = request.args
+        responseBody = json.dumps(resultData, sort_keys=False, indent=4, separators=(',', ': '))
+        #######    Replace this section by your logic   #######
+       
         return responseCode, responseBody
     
 #        request.write(message)
 #        request.finish()
-        
-    def postProcessing (self, message, request):
-        #######    Replace this section by your logic   #######
-        responseCode = ResponseCode.Ok
-        
-        result = {}
-        result ['method'] = 'post'
-        result ['path'] = request.path
-        result ['arguments'] = request.args
-        responseBody = json.dumps(result, sort_keys=False, indent=4, separators=(',', ': '))
-        #######    Replace this section by your logic   #######
-        return responseCode, responseBody
-        
-#        request.write(message)
-#        request.finish()
+    
     
     @classmethod
     def get(cls, request):
         responseCode = ResponseCode.Ok
         
-        url =  'http://127.0.0.1:8080/store/getOrder/orderId/10/item/2'
-        dbc1 = PreConditionOrder.dbcCondition(url, PreConditionOrder.callbackSucess, 200)
-        dbc1.addCallback (self.getProcessing, request)
+        dbc1 = defer.Deferred()
+        #url =  'http://127.0.0.1:8080/store/getOrder/orderId/10/item/2'
+        #dbc1 = PreConditionOrder.dbcCondition(url, PreConditionOrder.callbackSucess, 200)
+        dbc1.addCallback (Resource.preGetProcessing, request)
+        dbc1.addErrback     (Resource.preGetOtherwise, dbc1)
+        dbc1.addCallback (Resource.getProcessing, request)
         
-        return NOT_DONE_YET
- 
-#         
-#         if not ( request.args['orderId'] <> '0' ) :
-#             responseCode = ResponseCode.InvalidPrecondition
-#             return responseCode, "InvalidPrecondition"
-#         
-#         
-#         #######    Replace this section by your logic   #######
-#         result = {}
-#         result ['method'] = 'get'
-#         result ['path'] = request.path
-#         result ['arguments'] = request.args
-#         responseBody = json.dumps(result, sort_keys=False, indent=4, separators=(',', ': '))
-#         #######    Replace this section by your logic   #######
-#         return responseCode, responseBody
- 
-    @classmethod
-    def post(cls, request):
-        responseCode = ResponseCode.Ok
- 
-        url =  'http://127.0.0.1:8080/store/getOrder/orderId/10/item/2'
-        dbc1 = PreConditionOrder.dbcCondition(url, PreConditionOrder.callbackSucess, 200)
-        dbc1.addCallback (self.postProcessing, request)
+        dbc1.callback(request)
+                
+        return dbc1.result[0], dbc1.result[1]
         
-        return NOT_DONE_YET
+    
+     
+ 
+#    @classmethod
+#    def post(cls, request):
+#        responseCode = ResponseCode.Ok
+# 
+        #url =  'http://127.0.0.1:8080/store/getOrder/orderId/10/item/2'
+        #dbc1 = PreConditionOrder.dbcCondition(url, PreConditionOrder.callbackSucess, 200)
+        #dbc1.addCallback (self.postProcessing, request)
+        
+        #return NOT_DONE_YET
  
 
