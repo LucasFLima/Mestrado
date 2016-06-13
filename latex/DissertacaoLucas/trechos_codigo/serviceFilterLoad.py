8bf2cf999f227e45dbd6a874b165e9beedc1c3a3
Lucas Lima <lucas.f.lima@gmail.com>
2016-06-13 09:43:45 -0300

    Versão da dissertação do dia 13-06

--- latex/DissertacaoLucas/trechos_codigo/serviceFilterLoad.py
+++ latex/DissertacaoLucas/trechos_codigo/serviceFilterLoad.py
@@ -8,16 +8,12 @@ class Resource(object):
         
         # loads pre conditions filters
         dbcCondList.addFilterCondition(d, OperationType.GET, DbcConditionType.PreCondition, agent, request, args)
-  
         # call Get operation
         d.addCallback(ServiceXSrv.do_Get,      request, args)
-        
         # loads post conditions filters
         dbcCondList.addFilterCondition(d, OperationType.GET, DbcConditionType.PostCondition, agent, request, args)
-  
         # errorBack to return otherwise value if some filter fails
         d.addErrback (HandleOtherwise.handle, request)
         
         d.callback(utils.serviceResponse())
-        
         return d
